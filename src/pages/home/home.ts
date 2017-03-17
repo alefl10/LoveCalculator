37e11d07ca68e9b5fcae9c487d02ca3739c871b2
System Administrator <root@Alejandros-MacBook-Pro.local>
2017-03-17 12:11:38 -0400

    Full App operating

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 import { Contacts } from 'ionic-native';
-import { NavController } from 'ionic-angular';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-name1 = '';
-name2 = '';
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+  name1 = '';
+  name2 = '';
   get score() {
     const letters = (this.name1 + this.name2).toLowerCase();
     let sum = 0;
@@ -23,24 +15,22 @@ name2 = '';
     }
     return sum % 101;
   }
-
   picContact1() {
-    Contacts.pickContact().then(contact => {
-      this.name1 = contact.displayName;
-    }).catch(error => {
-  // ignore; user didn't select a contact
-    });
+    Contacts.pickContact()
+      .then(contact => {
+        this.name1 = contact.displayName;
+      })
+      .catch(error => {
+        // ignore; user didn't select a contact
+      });
   }
-
   picContact2() {
-  Contacts.pickContact()
-  .then(contact => {
-  this.name2 = contact.displayName;
-  })
-  .catch(error => {
-  // ignore; user didn't select a contact
-  });
+    Contacts.pickContact()
+      .then(contact => {
+        this.name2 = contact.displayName;
+      })
+      .catch(error => {
+        // ignore; user didn't select a contact
+      });
   }
-
-
 }
